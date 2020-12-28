693c7a1b2aa89e7d8f7238d0f4fbaf5b159aaf90
up1 <somkiat.p@gmail.com>
2020-12-29 01:13:09 +0700

    Controler + Repository

--- workshop/day2/src/main/java/com/workshop/day2/users/UserController.java
+++ workshop/day2/src/main/java/com/workshop/day2/users/UserController.java
@@ -1,24 +1,37 @@
 package com.workshop.day2.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/users")
     public List<UserResponse> getAllUsers() {
         List<UserResponse> userResponseList = new ArrayList<>();
-        userResponseList.add(new UserResponse(1,"demo 1", 30));
-        userResponseList.add(new UserResponse(2,"demo 2", 35));
+        List<User> users = userRepository.findAll();
+        for (User user : users) {
+            userResponseList.add(new UserResponse(user.getId(),
+                    user.getName(), user.getAge()));
+        }
         return userResponseList;
     }
 
     @GetMapping("/users/{id}")
     public UserResponse getUserById(@PathVariable int id) {
-        UserResponse userResponse = new UserResponse(id, "Demo", 40);
+        User user = userRepository.findById(id).orElseThrow(() -> new UserPrincipalNotFoundException(id))
+        UserResponse userResponse = new UserResponse(user.getId(),
+                user.getName(), user.getAge());
         return userResponse;
     }
 
@@ -33,15 +46,27 @@ public class UserController {
 
     @PutMapping("/users/{id}")
     public UserResponse updateUser(@RequestBody UserRequest newUser, @PathVariable int id) {
-        // TODO
-        // 1. find by id
-        // 2. found => update user
-        // 3. not found => ?? (create ? or throw error)
-        UserResponse updatedUserResponse = new UserResponse(
-                id,
-                newUser.getName(),
-                newUser.getAge());
-        return updatedUserResponse;
+        return userRepository.findById(id).map(user -> {
+            user.setName(newUser.getName());
+            user.setAge(newUser.getAge());
+            userRepository.save(user);
+
+            UserResponse updatedUserResponse = new UserResponse(
+                    id,
+                    newUser.getName(),
+                    newUser.getAge());
+            return updatedUserResponse;
+        }).orElseGet(() -> {
+            User user = new User();
+            user.setId(id);
+            user = userRepository.save(user);
+
+            UserResponse updatedUserResponse = new UserResponse(
+                    user.getId(),
+                    user.getName(),
+                    user.getAge());
+            return updatedUserResponse;
+        });
     }
 
     @DeleteMapping("/users/{id}")
