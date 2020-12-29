6cd561113ba004c7cb8e979fbaa8dc96bf201f34
up1 <somkiat.p@gmail.com>
2020-12-29 16:20:30 +0700

    Add jacoco

--- workshop/rest/src/main/java/com/workshop/rest/user/UserController.java
+++ workshop/rest/src/main/java/com/workshop/rest/user/UserController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class UserController {
 
