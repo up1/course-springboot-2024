2c4e9e969a6f472548bf458a0e1598071e0671b8
up1 <somkiat.p@gmail.com>
2020-12-29 13:39:44 +0700

    Add error handler

--- workshop/rest/src/main/java/com/workshop/rest/user/UserController.java
+++ workshop/rest/src/main/java/com/workshop/rest/user/UserController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +13,8 @@ public class UserController {
     @GetMapping("/user")
     public List<UserResponse> getAllUser() {
         List<UserResponse> userResponseList = new ArrayList<>();
-        userResponseList.add(new UserResponse(1,"demo 1", 30));
-        userResponseList.add(new UserResponse(2,"demo 2", 35));
+        userResponseList.add(new UserResponse(1, "demo 1", 30));
+        userResponseList.add(new UserResponse(2, "demo 2", 35));
         return userResponseList;
     }
 
@@ -27,5 +23,6 @@ public class UserController {
         UserResponse userResponse = new UserResponse(id, "demo", 30);
         return userResponse;
     }
+
 
 }
