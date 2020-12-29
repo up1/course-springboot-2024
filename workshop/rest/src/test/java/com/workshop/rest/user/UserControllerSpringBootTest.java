de5ae857d3812ca5ac528f1fce6554f430bd8a04
up1 <somkiat.p@gmail.com>
2020-12-29 15:16:25 +0700

    Add more test

--- workshop/rest/src/test/java/com/workshop/rest/user/UserControllerSpringBootTest.java
+++ workshop/rest/src/test/java/com/workshop/rest/user/UserControllerSpringBootTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -16,15 +18,24 @@ public class UserControllerSpringBootTest {
     private TestRestTemplate restTemplate;
 
     @Test
+    @DisplayName("ทำการตรวจสอบ การดึงข้อมูล user ทั้งหมด")
+    public void getAllUser() {
+        // Call API
+        List response = restTemplate.getForObject("/user", List.class);
+        // Assert
+        assertEquals(2, response.size());
+    }
+
+    @Test
     @DisplayName("ทำการตรวจสอบ การดึงข้อมูล user ด้วย id=1")
     public void getUser() {
         // Call API
         UserResponse response = restTemplate.getForObject("/user/1", UserResponse.class);
 
         // Assert
-        assertEquals(1,response.getId());
-        assertEquals("demo",response.getName());
-        assertEquals(30,response.getAge());
+        assertEquals(1, response.getId());
+        assertEquals("demo", response.getName());
+        assertEquals(30, response.getAge());
 
         // Quiz => equals() + hashcode()
         UserResponse expected = new UserResponse(1, "demo", 30);
