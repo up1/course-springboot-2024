8cdcf7b61a46ff22ae96af32d6ba778d4ee1166e
up1 <somkiat.p@gmail.com>
2020-12-29 17:19:40 +0700

    Fix test

--- workshop/rest/src/test/java/com/workshop/rest/user/UserControllerWebMvcTest.java
+++ workshop/rest/src/test/java/com/workshop/rest/user/UserControllerWebMvcTest.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,8 +21,14 @@ public class UserControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     public void getUser() throws Exception {
+        // Stub
+        UserModel testModel = new UserModel(1, "demo", 30);
+        when(userService.inquiryUserById(1)).thenReturn(testModel);
         // Call API
         MvcResult mvcResult = this.mvc.perform(get("/user/1")
                 .accept(MediaType.APPLICATION_JSON))
